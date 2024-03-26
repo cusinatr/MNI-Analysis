a9be6f1bcb9bc3b01c38b4b9720252d932736fd6
UniBe <riccardo.cusinato.1@gmail.com>
2024-03-26 13:01:06 +0100

    Update mni_sc.py

--- mni_sc.py
+++ mni_sc.py
@@ -15,13 +15,13 @@ import mni_utils as uti
 
 base_path = Path("F:\\iEEG_neural_dynamics\\MNIOpen")
 mmp_name = "MMP_in_MNI_symmetrical_1.nii.gz"
-# mmp_macro_name = "mmp_macro.csv"
-out_dir = "Results_SC_gamma"
+out_dir = "Results_SC_bispectrum"
 dist_bins = np.arange(0, 101, 10)  # bins to use as distances
-freq_band = True
+freq_band = False
 band_freqs = [40, 80]
-recompute = False
-fit_bins = True
+recompute = True
+fit_bins = False
+use_bispectrum = True
 
 ###
 # Create folder for storing results
@@ -30,7 +30,6 @@ fit_bins = True
 res_path = base_path.joinpath(out_dir)
 res_path.mkdir(parents=True, exist_ok=True)
 mmp_path = base_path.joinpath(mmp_name)
-# mmp_macro_path = base_path.joinpath(mmp_macro_name)
 
 ###
 # Import data
@@ -99,6 +98,7 @@ for stage in ["W", "N3", "R"]:
             mmp_path,
             freq_band=freq_band,
             band_freqs=band_freqs,
+            use_bispectrum=use_bispectrum,
         )
         # Save
         df_sc_stages[stage].to_csv(res_path.joinpath(f"SC_{stage}.csv"))
@@ -122,11 +122,16 @@ for stage in ["W", "N3", "R"]:
     # Fit exponential decay to spatial correlations
     ###
 
+    if use_bispectrum:
+        upper_bounds = (100, np.inf, np.inf)
+    else:
+        upper_bounds = (100, 1, 1)
+
     # First, fit to "global" profile
     if fit_bins:
-        popt, pcov = uti.fit_sc_bins(df_sc_bin_stages[stage])
+        popt, pcov = uti.fit_sc_bins(df_sc_bin_stages[stage], upper_bounds=upper_bounds)
     else:
-        popt, pcov = uti.fit_sc(df_sc_stages[stage])
+        popt, pcov = uti.fit_sc(df_sc_stages[stage], upper_bounds=upper_bounds)
     df_params = pd.DataFrame(popt.reshape(1, -1), columns=["k", "a", "b"])
     file_name = f"SC_{stage}_fit"
     file_name += "_bins" if fit_bins else ""
@@ -142,33 +147,13 @@ for stage in ["W", "N3", "R"]:
         ]
         if fit_bins:
             df_sc_bin_reg = uti.compute_sc_bin(df_sc_reg, bins=dist_bins)
-            popt, pcov = uti.fit_sc_bins(df_sc_bin_reg)
+            popt, pcov = uti.fit_sc_bins(df_sc_bin_reg, upper_bounds=upper_bounds)
         else:
-            popt, pcov = uti.fit_sc(df_sc_reg)
+            popt, pcov = uti.fit_sc(df_sc_reg, upper_bounds=upper_bounds)
         df_params.append(
             pd.DataFrame(popt.reshape(1, -1), columns=["k", "a", "b"], index=[reg])
         )
     df_params = pd.concat(df_params)
     file_name = f"SC_{stage}_fit_mni_regs"
-    file_name += "_bins" if fit_bins else ""
-    df_params.to_csv(res_path.joinpath(file_name + ".csv"))
-
-    # Then, on "corrected" CC value
-    df_params = []
-    for reg in df_sc_stages[stage]["region_1"].unique():
-        df_sc_reg = df_sc_stages[stage][
-            (df_sc_stages[stage]["region_1"] == reg)
-            | (df_sc_stages[stage]["region_2"] == reg)
-        ]
-        if fit_bins:
-            df_sc_bin_reg = uti.compute_sc_bin(df_sc_reg, bins=dist_bins)
-            popt, pcov = uti.fit_sc_bins(df_sc_bin_reg, col_name="corrc_max")
-        else:
-            popt, pcov = uti.fit_sc(df_sc_reg, col_name="corrc_max")
-        df_params.append(
-            pd.DataFrame(popt.reshape(1, -1), columns=["k", "a", "b"], index=[reg])
-        )
-    df_params = pd.concat(df_params)
-    file_name = f"SC_{stage}_fit_mni_regs_corrc"
     file_name += "_bins" if fit_bins else ""
     df_params.to_csv(res_path.joinpath(file_name + ".csv"))
