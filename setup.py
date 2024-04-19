4bfc07d5c1c785611cf9e57cbf1ed6879e6aece7
UniBe <riccardo.cusinato.1@gmail.com>
2024-04-19 15:20:57 +0200

    Adjust naming

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup, find_packages
 
 
-with open("README.rst") as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open("LICENSE") as f:
+with open("LICENSE.txt") as f:
     license = f.read()
 
 setup(
     name="mnitimescales",
-    version="0.1.0",
+    version="0.1.0-dev",
     description="Analysis of neural timescales in the MNI dataset",
     long_description=readme,
     author="Riccardo Cusinato",
     author_email="riccardo.cusinato@unibe.ch",
     url="https://github.com/cusinatr/MNI-Analysis",
     license=license,
-    packages=find_packages(include=("mnitimescales")),
+    packages=["mnitimescales"],
 )
